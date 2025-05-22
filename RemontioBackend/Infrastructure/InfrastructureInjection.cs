634a238a607def4c85b1f6177b2233e7eb8d1783
Yorri <zadlo.rafal.dev@gmail.com>
2025-05-22 15:50:55 +0200

    JWT

--- RemontioBackend/Infrastructure/InfrastructureInjection.cs
+++ RemontioBackend/Infrastructure/InfrastructureInjection.cs
@@ -1,10 +1,16 @@
-﻿using Domain.Entities;
+﻿using Application.Interfaces.ServiceInterfaces;
+using Domain.Entities;
 using Infrastructure.Data;
+using Infrastructure.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Text;
 
 namespace Infrastructure
 {
@@ -14,8 +20,11 @@ namespace Infrastructure
         {
             #region Scopes
 
+            services.AddScoped<ITokenService, TokenService>();
 
             #endregion
+
+            #region DB
 
             services.AddDbContext<RemontioDbContext>(options =>
                 options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
@@ -29,7 +38,43 @@ namespace Infrastructure
                 options.Password.RequireNonAlphanumeric = false;
                 options.Password.RequireUppercase = true;
                 options.Password.RequireLowercase = true;
-            }).AddEntityFrameworkStores<RemontioDbContext>(); 
+            }).AddEntityFrameworkStores<RemontioDbContext>();
+
+            #endregion
+
+            #region JWT
+
+            services.AddAuthentication(op =>
+            {
+                op.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                op.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = config["Jwt:Issuer"],
+                    ValidAudience = config["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]!))
+                };
+
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+
+            });
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("UserOnly", policy => policy.RequireClaim(ClaimTypes.NameIdentifier));
+            });
+
+            services.AddControllers();
+
+
+            #endregion 
 
         }
     }
---
+++ RemontioBackend/RemontioBackend.Presentation/Controllers/TokenController.cs
@@ -0,0 +1,32 @@
+﻿using Application.Interfaces.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TokenController : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+
+        public TokenController(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [HttpGet("generate-token")]
+        public IActionResult GenerateToken(/* need to add userDTO*/)
+        {
+            var token = _tokenService.GenerateToken(/* need to add userDTO*/);
+            return Ok(new { Token = token });
+        }
+
+        [HttpGet("generate-refresh-token")]
+        public IActionResult GenerateRefreshToken()
+        {
+            var refreshToken = _tokenService.GenerateRefreshToken();
+            return Ok(new { RefreshToken = refreshToken });
+        }
+
+    }
+}
